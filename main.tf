9582df1aa82374b9fab2cef82258607f47d39522
Dmitry Kireev <dmitry@hazelops.com>
2021-01-16 14:39:33 +0200

    Reformat, add EC2 features: volume mappings, port mapping

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ locals {
 
   environment = merge(var.environment,
     {
-      DD_APM_ENABLED = "true"
+      DD_APM_ENABLED                 = "true"
       DD_DOGSTATSD_NON_LOCAL_TRAFFIC = "true"
-      DD_APM_NON_LOCAL_TRAFFIC = "true"
-      DD_PROCESS_AGENT_ENABLED = "true"
-      DD_TAGS = "env:${var.env} app:${var.app_name}"
-      DD_TRACE_ANALYTICS_ENABLED = "true"
+      DD_APM_NON_LOCAL_TRAFFIC       = "true"
+      DD_PROCESS_AGENT_ENABLED       = "true"
+      DD_TAGS                        = "env:${var.env} app:${var.app_name}"
+      DD_TRACE_ANALYTICS_ENABLED     = "true"
 
       // https://www.datadoghq.com/blog/monitor-aws-fargate/
       ECS_FARGATE = var.ecs_launch_type == "FARGATE" ? "true" : "false"
@@ -20,44 +20,79 @@ locals {
   )
 
   container_definition = {
-    name              = var.name
-    image = "${var.docker_image_name}:${var.docker_image_tag}",
-    memoryReservation = 128,
-    essential = true,
+    name                 = var.name
+    image                = "${var.docker_image_name}:${var.docker_image_tag}",
+    memoryReservation    = 128,
+    essential            = true,
     resourceRequirements = var.resource_requirements
 
-    environment = [for k, v in local.environment : {name = k, value = v}]
-      secrets = module.ssm.secrets
+    environment = [for k, v in local.environment : { name = k, value = v }]
 
-      volumeMappings = var.ecs_launch_type == "FARGATE" ? [] : [
-        {
-          containerVolume = "/var/run/docker.sock",
-          hostVolume = "/var/run/docker.sock"
-        }
-      ],
+    secrets = module.ssm.secrets
 
+    mountPoints = var.ecs_launch_type == "FARGATE" ? [] : [
+      {
+        containerPath = "/var/run/docker.sock",
+        sourceVolume  = "docker_sock"
+      },
+      {
+        containerPath = "/host/sys/fs/cgroup",
+        sourceVolume  = "cgroup",
+        readOnly      = "true"
+      },
+      {
+        containerPath = "/host/proc",
+        sourceVolume  = "proc",
+        readOnly      = "true"
+      }
+    ]
 
+    volumes = var.ecs_launch_type == "FARGATE" ? [] : [
+      {
+        host = {
+          sourcePath = "/var/run/docker.sock"
+        },
+        name = "docker_sock"
+      },
+      {
+        host = {
+          sourcePath : "/proc/"
+        },
+        name = "proc"
+      },
+      {
+        host = {
+          sourcePath = "/cgroup/"
+        },
+        name = "cgroup"
+      }
+    ]
 
-      logConfiguration = var.cloudwatch_log_group == "" ? {
-        logDriver = "json-file"
-        options = {}
+    portMappings = var.ecs_launch_type == "FARGATE" ? [] : [
+      {
+        protocol      = "tcp",
+        containerPort = 8126
+      }
+    ]
+
+    logConfiguration = var.cloudwatch_log_group == "" ? {
+      logDriver = "json-file"
+      options   = {}
       } : {
-        logDriver = "awslogs",
-        options = {
-          awslogs-group = var.cloudwatch_log_group
-          awslogs-region = data.aws_region.current.name
-          awslogs-stream-prefix = var.name
-        }
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = var.cloudwatch_log_group
+        awslogs-region        = data.aws_region.current.name
+        awslogs-stream-prefix = var.name
       }
     }
-
-
+  }
 }
 
 module "ssm" {
-  source = "hazelops/ssm-secrets/aws"
-  version = "~> 1.0"
-  env = var.env
+  source   = "hazelops/ssm-secrets/aws"
+  version  = "~> 1.0"
+  env      = var.env
   app_name = var.app_name
-  names = var.enabled ? local.secret_names : []
+  names    = var.enabled ? local.secret_names : []
 }
